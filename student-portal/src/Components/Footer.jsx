a551da80532ac37fb9af3857da73a27f3616440b
Devansh Gupta <devansh2002gupta@gmail.com>
2024-12-05 12:27:58 +0530

    commit

--- student-portal/src/Components/Footer.jsx
+++ student-portal/src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <div>
-        <h1 className='text-zinc-500 absolute bottom-3 text-md left-[50%] -translate-x-[50%] -translate-y-[50%]'>@copyright 2024 one-step</h1>
+        <h1 className='text-zinc-500 fixed bottom-3 text-md left-[50%] -translate-x-[50%] -translate-y-[50%]'>@copyright 2024 one-step</h1>
       </div>
     </div>
   );
